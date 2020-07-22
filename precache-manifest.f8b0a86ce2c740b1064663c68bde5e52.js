69ae07ff8e2a2c362d89c9dab8fc4e522a046373
stanislav247 <ivanov.stanislav247@gmail.com>
2020-07-22 17:57:59 +0300

    Updates

---
+++ precache-manifest.f8b0a86ce2c740b1064663c68bde5e52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac82b992694975dc09a5f864c851c04c",
+    "url": "/guess-game/index.html"
+  },
+  {
+    "revision": "0d274f24f333d4710ac7",
+    "url": "/guess-game/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "4d358427e993c08b17c4",
+    "url": "/guess-game/static/css/main.bbc2a538.chunk.css"
+  },
+  {
+    "revision": "0d274f24f333d4710ac7",
+    "url": "/guess-game/static/js/2.eadcfa52.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/guess-game/static/js/2.eadcfa52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d358427e993c08b17c4",
+    "url": "/guess-game/static/js/main.1db26147.chunk.js"
+  },
+  {
+    "revision": "0eea13133a8e2e0f663f",
+    "url": "/guess-game/static/js/runtime-main.48018b24.js"
+  }
+]);
